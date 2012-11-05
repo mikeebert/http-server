c0c40c425dac95286cba4870f2747a3ea554c565
Mike Ebert <mike@mikeebert.net>
2012-11-05 14:32:40 -0600

    Prepares Response with Status Line and Spike 'hello world' body

--- src/HttpServer/Responder.java
+++ src/HttpServer/Responder.java
@@ -4,15 +4,34 @@ import java.io.PrintWriter;
 
 public class Responder {
 
+	private String preparedResponse;
+	private PrintWriter outputWriter;
+	private String body;
+
 	public Responder(PrintWriter writer) {
-		//To change body of created methods use File | Settings | File Templates.
+		outputWriter = writer;
 	}
 
 	public void prepare(Response response) {
+//		preparedResponse = statusLine(response);
+//			+ headers(response) + CRLF + response.getContent();
+
 		//package response
+		//set status line
+		//set headers
+		//insert CRLF
+		//insert response.content
+
 	}
 
-	public void respond() {
+	// spike code to generate a Hello World Page
+	public void sendResponse() {
+		body = "<!DOCTYPE html>\r\n<html>\r\n<head>\r\n</head>\r\n<body>\r\n<h1>\r\nHello World\r\n</h1>\r\n</body>\r\n</html>\r\n";
+		System.out.println("HTTP/1.1 200 OK" + "\r\n" + "<!doctype html><html><head></head><body><h1>Hello World</h1></body></html>");
+		outputWriter.println("HTTP/1.1 200 OK" + "\r\n" +
+												 "Content-Type: text/html" + "\r\n" +
+												 "Content-Length: " + body.length() + "\r\n\r\n" +
+													 body);
 		//needs to send response with the output
 	}
 }
--- test/HttpServer/ResponderTest.java
+++ test/HttpServer/ResponderTest.java
@@ -1,4 +1,14 @@
 package HttpServer;
 
+import org.junit.Test;
+
+import java.io.PrintWriter;
+
 public class ResponderTest {
+
+	@Test
+	public void itPreparesResponseWithStatusLine() throws Exception {
+		MockPrintWriter mockWriter = new MockPrintWriter(new MockOutputStream());
+		Responder responder = new Responder(mockWriter);
+	}
 }
