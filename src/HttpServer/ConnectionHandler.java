2c8b060ba06e37cfc2a99166752d700ea8ac02cc
Mike Ebert <mike@mikeebert.net>
2012-12-12 13:41:02 -0600

    Refactoring. Passing all CobSpec tests but image, echoback and redirect

--- src/HttpServer/ConnectionHandler.java
+++ src/HttpServer/ConnectionHandler.java
@@ -6,9 +6,10 @@ import java.net.Socket;
 
 public class ConnectionHandler {
 	private int port;
-	private ServerSocket server;
+	private ServerSocket socket;
 	private Socket clientSocket;
 	private RouterInterface router;
+	private ResponseBuilder builder;
 
 	public ConnectionHandler(int portNumber, RouterInterface appRouter) {
 		port = portNumber;
@@ -16,36 +17,24 @@ public class ConnectionHandler {
 	}
 
 	public void open() throws IOException {
-		server = new ServerSocket(port);
-		printStatus("Socket open on " + port);
+		socket = new ServerSocket(port);
+		printStatus("Listening on " + port);
 
-		handleNewClientSocket();
+		handleNewConnection();
 	}
 
-	private void handleNewClientSocket() {
+	private void handleNewConnection() {
 		while (true) {
 			try {
-			clientSocket = server.accept();
-			Request request = getRequest(clientSocket, input(clientSocket));
-			processResponse(responseTo(request), output(clientSocket));
-			printStatus("Closing socket");
-			clientSocket.close();
+				clientSocket = socket.accept();
+				Request request = getRequest(clientSocket, input(clientSocket));
+				Response response = routeRequest(request);
+				processResponse(response, output(clientSocket));
+				clientSocket.close();
 			} catch (Exception e) {
-				//should this do something?
+				e.printStackTrace();
 			}
 		}
-	}
-
-	private Request getRequest(Socket clientSocket, InputStream input) throws IOException {
-		RequestParser parser = new RequestParser(new BufferedReader(new InputStreamReader(input)));
-		return parser.parseRequest();
-	}
-
-	private void processResponse(Response response, OutputStream output) {
-		Responder responder = new Responder(new PrintWriter(new OutputStreamWriter(output), true));
-		//could-should separate the following two responsibilities
-		responder.prepare(response);
-		responder.sendResponse();
 	}
 
 	private InputStream input(Socket clientSocket) throws IOException {
@@ -56,7 +45,23 @@ public class ConnectionHandler {
 	  return clientSocket.getOutputStream();
 	}
 
-	private Response responseTo(Request request) throws IOException {
+	private Request getRequest(Socket clientSocket, InputStream input) throws IOException {
+		RequestParser parser = new RequestParser(new BufferedReader(new InputStreamReader(input)));
+		return parser.parseRequest();
+	}
+
+	private void processResponse(Response response, OutputStream output) {
+		Responder responder = new Responder(new PrintWriter(new OutputStreamWriter(output), true));
+		responder.prepare(response);
+		responder.sendResponse();
+
+//		builder = new ResponseBuilder(response);
+//		Response preparedResponse = builder.build(response);
+//    ResponseSender sender = new ResponseSender(new PrintWriter(new OutputStreamWriter(output), true));
+//		sender.sendResponse(preparedResponse);
+	}
+
+	private Response routeRequest(Request request) throws IOException {
 		return router.setResponseFor(request);
 	}
 
@@ -64,8 +69,12 @@ public class ConnectionHandler {
 		return port;
 	}
 
-	public ServerSocket getServer() {
-		return server;
+	public ServerSocket getSocket() {
+		return socket;
+	}
+
+	public void setSocket(ServerSocket newSocket) {
+		socket = newSocket;
 	}
 
 	private void printStatus(String status) {
