a809ef193658bb3e497b8f9c3fc1b2267cd57606
Mike Ebert <mike@mikeebert.net>
2012-12-13 16:24:19 -0600

    Update Router to be Generic and take a routes file.

---
+++ src/HttpServer/Router.java
@@ -0,0 +1,85 @@
+package HttpServer;
+
+import HttpServer.Request;
+import HttpServer.Response;
+import HttpServer.RouterInterface;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Router {
+	private String dir;
+	private List<HashMap<String, String>> routes = new ArrayList<HashMap<String, String>>();
+
+	public Router(String directory) {
+		setDirectory(directory);
+		try {
+			setRoutes();
+		} catch (IOException e) {
+			System.out.println("Error reading routes file.");
+		}
+	}
+
+	private void setRoutes() throws IOException {
+		String[] routesArray = readFile(dir + "routes.txt").split("\n");
+		int routes_header_row = 0;
+
+		for (int i= routes_header_row + 1; i < routesArray.length; i++) {
+			String[] route = routesArray[i].split(" \\| ");
+
+			HashMap<String, String> pathResource = new HashMap<String, String>();
+			pathResource.put("path", route[0]);
+			pathResource.put("verb", route[1]);
+			pathResource.put("resource", route[2]);
+			routes.add(pathResource);
+		}
+	}
+
+//	private boolean routesContain(String path) {
+//		for(HashMap route: routes ) {
+//			if(route.get("path").equals(path)) {
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+
+	public String getResourceFor(String path) {
+
+		for(HashMap route: routes) {
+			System.out.println("Trying to match: " + path + " to: " + route.get("path"));
+			if(route.get("path").equals(path))
+				return dir + route.get("resource").toString();
+		}
+
+		return dir + "404.html";
+	}
+
+	private String readFile(String filePath) throws IOException {
+		FileInputStream fileStream = new FileInputStream(filePath);
+		BufferedReader fileReader = new BufferedReader(new InputStreamReader(fileStream));
+		StringBuilder input = new StringBuilder();
+		String line = fileReader.readLine();
+
+		while ((line != null) && (!line.equals(""))) {
+			input.append(line).append("\n");
+			line = fileReader.readLine();
+		}
+
+		fileStream.close();
+		return input.toString();
+	}
+
+	public List<HashMap<String, String>> getRoutes() {
+		return routes;
+	}
+
+	private void setDirectory(String directory) {
+		dir = directory;
+	}
+}
--- test/HttpServer/RouterTest.java
+++ test/HttpServer/RouterTest.java
@@ -1,18 +1,43 @@
 package HttpServer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
-
 public class RouterTest {
-//	@Test
-//	public void itReturns() throws Exception {
-//		Request request = new Request();
-//		request.setBody("This is the body");
-//
-//		Router router = new Router();
-//		Response response = router.getResponseFor(request);
-//		assertEquals("This is the body.", response.getContent());
-//	}
+	private static final String DIR = "/Users/ebert/Dropbox/projects/http-server/test/HttpServer/";
+	private Router router;
+
+	@Before
+	public void setUp() throws Exception {
+		router = new Router(DIR);
+	}
+
+	@Test
+	public void readsRoutesFromFile() throws Exception {
+		List<HashMap<String, String>> routesList = new ArrayList<HashMap<String, String>>();
+		HashMap<String, String> routesHash = new HashMap<String, String>();
+		routesHash.put("path", "/");
+		routesHash.put("verb", "GET");
+		routesHash.put("resource", "index.html");
+		routesList.add(routesHash);
+
+		assertEquals(routesList, router.getRoutes());
+	}
+
+	@Test
+	public void itReturnsResourcePathInRoutes() throws Exception {
+		assertEquals(DIR + "index.html", router.getResourceFor("/"));
+	}
+
+	@Test
+	public void itReturns404ResourceForPathNotFound() throws Exception {
+		assertEquals(DIR + "404.html", router.getResourceFor("nonexistantpath"));
+	}
+
 }
