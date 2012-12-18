8b2930849ecf3e1900fe60ed03929d3013b6e537
Mike Ebert <mike@mikeebert.net>
2012-12-18 13:56:02 -0600

    update Mocks

--- test/HttpServer/MockBufferedReader.java
+++ test/HttpServer/MockBufferedReader.java
@@ -8,6 +8,8 @@ public class MockBufferedReader extends BufferedReader {
 	private String[] mockInput = new String[100];
 	private int readLineCount = 0;
 	private int inputLineCount = 0;
+	private String mockCharacter;
+	private boolean fetchedPostContent;
 
 	public MockBufferedReader(Reader in) {
 		super(in);
@@ -19,5 +21,23 @@ public class MockBufferedReader extends BufferedReader {
 
 	public void setMockInput(String string) {
 		mockInput[inputLineCount++] = string;
+	}
+
+	@Override
+	public int read(char[] characters, int buffer, int length) {
+		fetchedPostContent = true;
+		return length;
+	}
+
+	public String getMockCharacter() {
+		return mockCharacter;
+	}
+
+	public void setMockCharacter(String mockCharacter) {
+		this.mockCharacter = mockCharacter;
+	}
+
+	public boolean fetchedPostContent() {
+		return fetchedPostContent;
 	}
 }
