9ee090a330e70fac34107f65c7501027a4ea94ee
Mike Ebert <mike@mikeebert.net>
2012-12-19 08:51:12 -0600

    Adds MockFileReader

---
+++ test/HttpServer/MockFileReader.java
@@ -0,0 +1,12 @@
+package HttpServer;
+
+import java.io.IOException;
+
+public class MockFileReader extends FileReader {
+
+	public String fileContents;
+
+	public String readFile(String filePath) {
+		return fileContents;
+	}
+}
