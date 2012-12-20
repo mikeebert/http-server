312ef3b4c098cf758f0deb7d9a8e9e67c360fbc4
Mike Ebert <mike@mikeebert.net>
2012-12-20 14:51:59 -0600

    New and Update actions return updated HTML strings

--- src/tictactoe/GameController.java
+++ src/tictactoe/GameController.java
@@ -6,12 +6,16 @@ import HttpServer.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.mebert.ttt.Game;
+
 public class GameController implements ControllerInterface {
+	private static final String controllerName = "/game/";
+	private static final int[] SPACES = {1,2,3,4,5,6,7,8,9};
 
 	private boolean initialized = false;
 	private String resourceContent = "";
 	private FileReader fileReader;
-	private static final int[] SPACES = {1,2,3,4,5,6,7,8,9};
+
 
 	public GameController() {
 		setInitialized(true);
@@ -19,14 +23,14 @@ public class GameController implements ControllerInterface {
 	}
 
 	public String process(String resource, HashMap<String, String> params) throws IOException {
-		String resourceDirectory = resource.split("/game/")[0];
-		String method = resource.split("/game/")[1];
+		String resourceDirectory = resource.split(controllerName)[0];
+		String method = resource.split(controllerName)[1];
 
-		getResource(method, resourceDirectory, params);
+		updateResourceContent(method, resourceDirectory, params);
 		return resourceContent;
 	}
 
-	private void getResource(String method, String resourceDirectory, HashMap<String, String> params) throws IOException {
+	private void updateResourceContent(String method, String resourceDirectory, HashMap<String, String> params) throws IOException {
 		if(method.equals("new"))
 			resourceContent = newGame(resourceDirectory);
 		else if (method.equals("update"))
@@ -34,19 +38,69 @@ public class GameController implements ControllerInterface {
 	}
 
 	private String newGame(String resourceDirectory) throws IOException {
+		Game game = new Game();
+		GameRepository.store(game);
 		String boardFile = fileReader.readFile(resourceDirectory + "/board.html");
+		String newBoard = addGameId(boardFile, game.getId());
 
+		return  buildNewBoard(newBoard);
+	}
+
+	private String updateGame(String resourceDirectory, HashMap<String, String> params) throws IOException {
+		Game game = GameRepository.fetch(Integer.parseInt(params.get("game-id")));
+		String gameBoardHtml = updateBoardHtml(resourceDirectory, game.getId(), game.updateBoardWith(params.get("move")));
+
+		if (game.isOver())
+			return buildGameOverHtml(gameBoardHtml);
+		else
+			return gameBoardHtml;
+	}
+
+	private String buildNewBoard(String boardFile) {
 		for(int space: SPACES) {
-			boardFile = boardFile.replace("&&cell" + space,
-																		"<button name='cell" + space + "' value='on' type='submit'>move</button>");
+			boardFile = boardFile.replace("&&cell" + space, buildButtonFor(space));
 		}
-
 		return boardFile;
 	}
 
-	private String updateGame(String resourceDirectory, HashMap<String, String> params) {
+	private String updateBoardHtml(String resourceDirectory, int gameId, String[] moves) throws IOException {
+		String boardFile = fileReader.readFile(resourceDirectory + "/board.html");
+		String gameBoardFile = boardFile.replace("&&gameId", String.valueOf(gameId));
 
-		return resourceContent;
+		return buildUpdatedBoard(moves, gameBoardFile);
+	}
+
+	private String buildUpdatedBoard(String[] updatedMoves, String boardFile) {
+		for (int i = 0;i < updatedMoves.length; i++) {
+			if (isEmpty(updatedMoves[i])) {
+				boardFile = boardFile.replace("&&cell" + move(i), buildButtonFor(move(i)));
+			} else {
+				boardFile = boardFile.replace("&&cell" + move(i), updatedMoves[i]);
+			}
+		}
+		return boardFile;
+	}
+
+	private String buildGameOverHtml(String gameBoardHtml) {
+		String noButtons = gameBoardHtml.replaceAll("<button.*</button>", "");
+		String gameOverHtml = noButtons.replace("<p>Game In Progress</p>", "<p>Game Over</p>");
+		return gameOverHtml;
+	}
+
+	private String buildButtonFor(int move) {
+		return "<button name='move' value='" + move + "' type='submit'>move</button>";
+	}
+
+	private int move(int i) {
+		return 1 + i;
+	}
+
+	private boolean isEmpty(String move) {
+		return !move.equals("X") && !move.equals("O");
+	}
+
+	private String addGameId(String boardHtml, int gameId) {
+		return boardHtml.replace("&&gameId", String.valueOf(gameId));
 	}
 
 	@Override
--- test/tictactoe/GameControllerTest.java
+++ test/tictactoe/GameControllerTest.java
@@ -1,36 +1,69 @@
 package tictactoe;
 
 import HttpServer.MockFileReader;
+import org.junit.Before;
 import org.junit.Test;
+import org.mebert.ttt.Board;
+import org.mebert.ttt.Game;
+import org.mebert.ttt.MockBoard;
 
 import java.util.HashMap;
 import static junit.framework.Assert.assertEquals;
 
 public class GameControllerTest {
+	private GameController controller;
+	private MockFileReader mockReader;
 
-	@Test
-	public void itReturnsNewBoard() throws Exception {
-		GameController controller = new GameController();
-		MockFileReader mockReader = new MockFileReader();
-		mockReader.fileContents = "test";
-
+	@Before
+	public void setUp() {
+		GameRepository.initialize();
+		controller = new GameController();
+		mockReader = new MockFileReader();
 		controller.setFileReader(mockReader);
-		HashMap<String, String> params = new HashMap<String, String>();
-
-		assertEquals("test", controller.process("/something/game/new", params));
 	}
 
 	@Test
-	public void itUpdatesBoardFile() throws Exception {
-		GameController controller = new GameController();
-		MockFileReader mockReader = new MockFileReader();
+	public void itReturnsNewBoard() throws Exception {
+		mockReader.fileContents = "test";
+
+		assertEquals("test", controller.process("/something/game/new", null));
+	}
+
+	@Test
+	public void itUpdatesBoardHtml() throws Exception {
 		mockReader.fileContents = "&&cell1";
-
-		controller.setFileReader(mockReader);
 		controller.process("/game/new", null);
-
 		String updatedContent = controller.getResourceContent();
 
-		assertEquals("<button name='cell1' value='on' type='submit'>move</button>", updatedContent);
+		assertEquals("<button name='move' value='1' type='submit'>move</button>", updatedContent);
+	}
+
+	@Test
+	public void itPassesParamsToUpdateExistingGame() throws Exception {
+		MockGame mockGame = new MockGame();
+		GameRepository.store(mockGame);
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("game-id", "1");
+		params.put("move", "1");
+
+		controller.process("some-directory/game/update", params);
+
+		assertEquals("1", mockGame.getReceivedMove());
+	}
+
+	@Test
+	public void itUpdatesStringContentForMovesAndID() throws Exception {
+		MockGame mockGame = new MockGame();
+		GameRepository.store(mockGame);
+		mockReader.fileContents = "&&cell1 | &&cell2 | &&cell3 | &&gameId";
+		mockGame.setMockMoves(new String[]{"X", "2", "O"});
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("game-id", "1");
+		params.put("move", "1");
+		String expectedOutput = "X | <button name='move' value='2' type='submit'>move</button> | O | 1";
+
+		controller.process("some-directory/game/update", params);
+
+		assertEquals(expectedOutput, controller.getResourceContent());
 	}
 }
